4fa386c49f47d6a2fb822065bd6514a57a520f25
Frank Emeli <dev.carlfrank@gmail.com>
2019-12-16 09:54:41 +0100

    Unauthorized users are now denied access to protected routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,16 +12,18 @@ func (app *application) routes() http.Handler {
 
 	mux := pat.New()
 	mux.Get("/", dynamicMiddleware.Then(http.HandlerFunc(app.home)))
-	mux.Get("/snippet/create", dynamicMiddleware.Then(http.HandlerFunc(app.createSnippetForm)))
-	mux.Post("/snippet/create", dynamicMiddleware.Then(http.HandlerFunc(app.creteSnippet)))
+	mux.Get("/snippet/create", dynamicMiddleware.Append(app.requireAuthenticatedUser).
+		ThenFunc(app.createSnippetForm))
+	mux.Post("/snippet/create", dynamicMiddleware.Append(app.requireAuthenticatedUser).
+		Then(http.HandlerFunc(app.creteSnippet)))
 	mux.Get("/snippet/:id", dynamicMiddleware.Then(http.HandlerFunc(app.showSnippet)))
 	mux.Get("/test", dynamicMiddleware.Then(http.HandlerFunc(app.test)))
-
 	mux.Get("/user/signup", dynamicMiddleware.ThenFunc(app.signupUserForm))
 	mux.Post("/user/signup", dynamicMiddleware.ThenFunc(app.signupUser))
 	mux.Get("/user/login", dynamicMiddleware.ThenFunc(app.loginUserForm))
 	mux.Post("/user/login", dynamicMiddleware.ThenFunc(app.loginUser))
-	mux.Post("/user/logout", dynamicMiddleware.ThenFunc(app.logoutUser))
+	mux.Post("/user/logout", dynamicMiddleware.Append(app.requireAuthenticatedUser).
+		ThenFunc(app.logoutUser))
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Get("/static/", http.StripPrefix("/static", fileServer))

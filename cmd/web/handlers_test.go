d2e347ab8c7907b7140207a1f98dadac2111c968
Carlfrank Emeli <dev.carlfrank@gmail.com>
2020-01-19 22:17:55 +0100

    snippet end-to-end test complete

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,14 +1,28 @@
 package main
 
 import (
-	"io/ioutil"
+	"bytes"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
 func TestPing(t *testing.T) {
-	rr := httptest.NewRecorder()
+	app := newTestApplication(t)
+
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	code, _, body := ts.Get(t, "/ping")
+
+	if code != http.StatusOK {
+		t.Errorf("want %d; got %d", http.StatusOK, code)
+	}
+
+	if string(body) != "OK" {
+		t.Errorf("want body to equal %q", "OK")
+	}
+
+	/*rr := httptest.NewRecorder()
 
 	r, err := http.NewRequest("GET", "/", nil)
 	if err != nil {
@@ -31,5 +45,39 @@ func TestPing(t *testing.T) {
 
 	if string(body) != "OK" {
 		t.Errorf("want body to equal %q", "OK")
+	}*/
+}
+
+func TestShowSnippet(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+	tests := []struct{
+		name string
+		urlPath  string
+		wantCode int
+		wantBody []byte
+	}{
+		{"Valid ID", "/snippet/1", http.StatusOK, []byte("An old silent pond...")},
+		{"Non-existent ID", "/snippet/2", http.StatusNotFound, nil},
+		{"Negative ID", "/snippet/-1", http.StatusNotFound, nil},
+		{"Decimal ID", "/snippet/1.23", http.StatusNotFound, nil},
+		{"String ID", "/snippet/foo", http.StatusNotFound, nil},
+		{"Empty ID", "/snippet/", http.StatusNotFound, nil},
+		{"Trailing slash", "/snippet/1/", http.StatusNotFound, nil},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			code, _, body := ts.Get(t, tt.urlPath)
+
+			if code != tt.wantCode {
+				t.Errorf("want %d; got %d", tt.wantCode, code)
+			}
+
+			if !bytes.Contains(body, tt.wantBody) {
+				t.Errorf("want body to contain %q", tt.wantBody)
+			}
+		})
 	}
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/tls"
 	"database/sql"
+	"emeli/snippetbox/pkg/models"
 	"github.com/golangcollege/sessions"
 	"html/template"
 	"time"
@@ -24,9 +25,17 @@ type application struct {
 	errorLog *log.Logger
 	infoLog *log.Logger
 	session *sessions.Session
-	snippets *mysql.SnippetModel
+	snippets interface {
+		Insert(string, string, string) (int, error)
+		Get(int) (*models.Snippet, error)
+		Latest() ([]*models.Snippet, error)
+	}
 	templateCache map[string]*template.Template
-	users *mysql.UserModel
+	users interface {
+		Insert(string, string, string) error
+		Authenticate(string, string) (int, error)
+		Get(int) (*models.User, error)
+	}
 }
 
 func main() {
